2e7718cb69b36771932520d2214e95c16cca23f1
Andrei Petrushin <andrei.petrushin@gmail.com>
2021-04-13 02:26:43 +0300

    fix: fix task

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,16 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const splitMAC = n.split('-');
+
+  if (splitMAC.length !== 6) return false;
+
+  return splitMAC.every((item) => {
+    const dec = parseInt(item, 16);
+
+    return dec >= 0 && dec <=255;
+  });
 }
 
 module.exports = isMAC48Address;
